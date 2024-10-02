7a76898a8c7cd494d3cc263d881841b5b47a4097
digiheadway <digiheadway@gmail.com>
2024-10-02 19:07:49 +0530

    imp

--- php/send_otp.php
+++ php/send_otp.php
@@ -1,4 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+
 session_start(); // Start the session
 
 // Function to check if the user has exceeded the request limit
--- php/verify_otp.php
+++ php/verify_otp.php
@@ -1,4 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+// Set the content type to JSON
+header('Content-Type: application/json');
+
 session_start(); // Start the session
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
