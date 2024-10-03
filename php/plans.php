d6f29a5cfbf2292ff410e72282933877302a840d
digiheadway <digiheadway@gmail.com>
2024-10-03 20:05:05 +0530

    imp

--- php/plans.php
+++ php/plans.php
@@ -5,9 +5,9 @@ header("Access-Control-Allow-Headers: Content-Type");
 // Example: Fetching plans from a database
 $meal_plans = [
     ["id" => "1", "type" => "Veg", "name" => "Plan 1", "items" => "4 Roti, Sabji 1, Sabji 2", "price" => 100, "frequency" => "Daily", "time" => "Lunch", "description" => "this is description"],
-    ["id" => "2", "type" => "Non Veg", "name" => "Plan 2", "items" => "4 Roti, Chicken Curry", "price" => 200, "frequency" => "Daily", "time" => "Lunch", "description" => "this is description"],
+    ["id" => "2", "type" => "NonVeg", "name" => "Plan 2", "items" => "4 Roti, Chicken Curry", "price" => 200, "frequency" => "Daily", "time" => "Lunch", "description" => "this is description"],
     ["id" => "3", "type" => "Veg", "name" => "Plan 3", "items" => "4 Roti, Sabji 1, Sabji 2", "price" => 300, "frequency" => "Daily", "time" => "Lunch", "description" => "this is description"],
-    ["id" => "4", "type" => "Non Veg", "name" => "Plan 4", "items" => "4 Roti, Chicken Curry", "price" => 400, "frequency" => "Daily", "time" => "Lunch", "description" => "this is description"],
+    ["id" => "4", "type" => "NonVeg", "name" => "Plan 4", "items" => "4 Roti, Chicken Curry", "price" => 400, "frequency" => "Daily", "time" => "Lunch", "description" => "this is description"],
     // Add more plans as needed
 ];
 
