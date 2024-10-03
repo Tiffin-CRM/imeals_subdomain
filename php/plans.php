eb9e15826dd53cd0e0b2e111617fcd8554b3e140
digiheadway <digiheadway@gmail.com>
2024-10-03 19:35:21 +0530

    imp

--- php/plans.php
+++ php/plans.php
@@ -1,8 +1,10 @@
 <?php
 // Example: Fetching plans from a database
 $meal_plans = [
-    ["id" => "plan3", "type" => "veg", "name" => "Plan 3", "description" => "4 Roti, Sabji 1, Sabji 2", "price" => 200, "meal" => "Daily Lunch"],
-    ["id" => "plan4", "type" => "nonveg", "name" => "Plan 4", "description" => "4 Roti, Chicken Curry", "price" => 250, "meal" => "Daily Lunch"],
+    ["id" => "plan1", "type" => "veg", "name" => "Plan 1", "description" => "4 Roti, Sabji 1, Sabji 2", "price" => 100, "meal" => "Daily Lunch"],
+    ["id" => "plan2", "type" => "nonveg", "name" => "Plan 2", "description" => "4 Roti, Chicken Curry", "price" => 200, "meal" => "Daily Lunch"],
+    ["id" => "plan3", "type" => "veg", "name" => "Plan 3", "description" => "4 Roti, Sabji 1, Sabji 2", "price" => 300, "meal" => "Daily Dinner"],
+    ["id" => "plan4", "type" => "nonveg", "name" => "Plan 4", "description" => "4 Roti, Chicken Curry", "price" => 400, "meal" => "Daily Breakfast"],
     // Add more plans as needed
 ];
 
