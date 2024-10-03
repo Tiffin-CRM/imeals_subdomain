625b87a16c6ed8a961ae2b91f9f85289e075ed4b
digiheadway <digiheadway@gmail.com>
2024-10-03 17:47:48 +0530

    imp

--- assets/login_script.js
+++ assets/login_script.js
@@ -28,18 +28,19 @@ function account_not_found() {
   overlay_btn.style.display = "block";
 }
 function check_if_account() {
-  var phoneNumber = localStorage.getItem("phone");
-  var token = localStorage.getItem("token");
+  // var phoneNumber = localStorage.getItem("phone");
+  var phoneNumber = document.getElementById("phoneNumber").value;
+  // var token = localStorage.getItem("token");
 
   // Ensure phoneNumber and token are available
-  if (!phoneNumber || !token) {
+  if (!phoneNumber) {
     alert("Phone number or token is missing.");
     return;
   }
 
   fetch("/php/check_account.php", {
     method: "POST",
-    body: JSON.stringify({ phone: phoneNumber, token: token }),
+    body: JSON.stringify({ phone: phoneNumber }),
     headers: {
       "Content-Type": "application/json",
     },
