ede9464888f9ea21964f82021e272e08c65b5e43
digiheadway <digiheadway@gmail.com>
2024-10-16 16:07:54 +0530

    imp

--- php/backend_add_plan.php
+++ php/backend_add_plan.php
@@ -1,7 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Origin: http://127.0.0.1:3000");
-
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
